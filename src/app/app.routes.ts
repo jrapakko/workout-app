706b1ce3245b7e8b8ec148be7a535a5ff41fdbc8
Janne Rapakko <jtrapakko@ucsd.edu>
2024-06-03 13:39:15 -0700

    added all workouts view to router, needs styling and functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'workouts', component: WorkoutsComponent }
 ];
--- src/app/workouts/workouts.component.ts
+++ src/app/workouts/workouts.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Regimen, Workout } from '../workout';
+import { WorkoutService } from '../workout.service';
+import { WorkoutCardComponent } from '../workout-card/workout-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-workouts',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    WorkoutCardComponent
+  ],
   templateUrl: './workouts.component.html',
   styleUrl: './workouts.component.css'
 })
 export class WorkoutsComponent {
 
+  regimen!: Regimen;
+  workoutService: WorkoutService = inject(WorkoutService);
+
+  constructor() {
+    this.regimen = this.workoutService.getRegimen();
+  }
 }
