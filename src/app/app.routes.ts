3117ca25f76a864d09a48d01cd480c65384884b6
Janne Rapakko <jtrapakko@ucsd.edu>
2024-07-30 15:29:55 -0700

    add workout file addition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 import { RegimenComponent } from './regimen/regimen.component';
+import { WorkoutFormComponent } from './workout-form/workout-form.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'workouts', component: WorkoutsComponent },
-    { path: 'routine', component: RegimenComponent }
+    { path: 'routine', component: RegimenComponent },
+    { path: 'add-workout', component: WorkoutFormComponent }
 ];
