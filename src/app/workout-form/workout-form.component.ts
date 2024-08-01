390d933d16ebdfa229fbe81e8dca399902924455
Janne Rapakko <jtrapakko@ucsd.edu>
2024-08-01 15:06:13 -0700

    basic form group with array for exercises for adding workouts

--- src/app/workout-form/workout-form.component.ts
+++ src/app/workout-form/workout-form.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Workout, Exercise, ExerciseSet } from '../workout';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-workout-form',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    ReactiveFormsModule
+  ],
   templateUrl: './workout-form.component.html',
   styleUrl: './workout-form.component.css'
 })
 export class WorkoutFormComponent {
 
+  
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  workoutForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    exercises: this.formBuilder.array([])
+  });
+
+  get exercises() {
+    return this.workoutForm.get('exercises') as FormArray;
+  }
+
+  newExercise(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      sets: 0,
+      reps: 0
+    });
+ }
+
+  addExercise() {
+    this.exercises.push(this.newExercise());
+  }
+
+  removeExercise(i: number) {
+    this.exercises.removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.workoutForm.value);
+  }
 }
